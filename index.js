0f859a280707bc6d3d23d14d1e41185007fa2f5a
Tommy Williams <tom@teow.co.uk>
2016-10-12 16:09:57 +0100

    tommy updates

--- index.js
+++ index.js
@@ -16,39 +16,58 @@ firebase.initializeApp(config);
 const ref = firebase.database().ref();
 
 var friendsTokensArray = [];
+var friendsFacebookIdsArrayTest = ['10154479520857387',  '10157474917765252',  '10153684774890666',  '10154522964878397',  '10155322526784782']
 
 function listenForNotificationRequests() {
   const notifications = ref.child('notifications');
   notifications.on('child_added', (notificationSnapshot) => {
     const notification = notificationSnapshot.val();
     // console.log(notification.friendsFacebookIds);
-      convertFacebookIdsToTokens(notification.friendsFacebookIds)
-      friendsTokensArray.forEach(token => {
-        sendNotificationToUser(
-          token,
-          notification.subtitle,
-          notification.title,
-          () => {
-            // requestSnapshot.ref.remove();
-          }
-        );
-      })
+    getFriendsFacebookIds(
+      convertFacebookIdsToTokens(friendsFacebookIds,
+        eachSendNotificationToUser(friendsTokensArray)
+      )
+    )
   }, (error) => {
     console.error(error);
   });
 }
 
-function convertFacebookIdsToTokens(friendsFacebookIds) {
+function eachSendNotificationToUser(friendsTokensArray) {
+  friendsTokensArray.forEach(token => {
+    sendNotificationToUser(
+      token,
+      notification.subtitle,
+      notification.title,
+      () => {
+        // requestSnapshot.ref.remove();
+      }
+    );
+  })
+}
+
+function getFriendsFacebookIds(callback) {
+  const notifications = ref.child('notifications');
+  notifications.on('value', (notificationSnapshot) => {
+    const notification = notificationSnapshot.val();
+  })
+  console.log(notification.friendsFacebookIds);
+  callback(notification.friendsFacebookIds)
+}
+
+function convertFacebookIdsToTokens(friendsFacebookIds, callback) {
   console.log(friendsFacebookIds);
   friendsFacebookIds.forEach(id => {
     const tokens = ref.child('tokens');
     tokens.on('value', (tokenSnapshot) => {
       // console.log(tokenSnapshot.val());
       const token = tokenSnapshot.val()
-      // console.log(token[id].tokenID);
+      console.log(token[id]);
+      console.log(token[id].tokenID);
       friendsTokensArray.push(token[id].tokenID);
-      // console.log(friendsTokensArray);
+      console.log(friendsTokensArray);
     })
+    callback(friendsTokensArray)
   });
 }
 
@@ -69,7 +88,7 @@ function sendNotificationToUser(username, title, message, onSuccess) {
             title: title,
             body: message
           },
-          to: "djZle4-WuPo:APA91bFEk6rSIKktD-LUBwN0ii-pTb9DGAN87a90kc6tRMvz59--pivjpq3BvyfUc_wiksPbHEUEAoAdfHqeS6jpLpBof5CS-b0wK47jRQ1KNvRNykfq42avGv1JhkjxflJYoIS8_LjB",
+          to: username,
           priority: 'high',
           content_available: true
         })
